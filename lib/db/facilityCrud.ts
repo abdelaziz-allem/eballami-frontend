39b4e6a74e11dbdefdc901f2192d37059fb21864
Abdelaziz Allem <abdelaziz.allem@gmail.com>
2025-02-16 12:56:04 +0300

    fix

--- lib/db/facilityCrud.ts
+++ lib/db/facilityCrud.ts
@@ -39,7 +39,7 @@ export const createFacility = async (
     });
     return response.data;
   } catch (error) {
-    console.error("Error creating facility:", error);
+    console.error("Error creating facility:", error.response.data.message);
     throw new Error("Failed to create facility");
   }
 };
